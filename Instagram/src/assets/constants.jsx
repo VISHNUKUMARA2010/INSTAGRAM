aa71ce86f1e313598bd69c5015d9c77216ffb82c
VISHNUKUMARA2010 <vishnukumar.a2010@gmail.com>
2026-03-27 22:14:09 +0530

    feat: added core interactive components - 17h 31m total

---
+++ Instagram/src/assets/constants.jsx
@@ -0,0 +1,13 @@
+export const InstagramLogo = () => (
+    <svg
+        aria-lable='Instagram'
+        color='rgb(245, 245, 245)'
+        fill='rgb(245, 245, 245)'
+        height='29'
+        role='img'
+        viewBox='32 4 113 32'
+        width='103'
+    >
+        
+    </svg>
+)
